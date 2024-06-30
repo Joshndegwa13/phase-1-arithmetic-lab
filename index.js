4a92d64cf2be487ded6f2c619deeab9ce64ee86d
Joshndegwa13 <ndegwajosh7@gmail.com>
2024-06-30 11:52:39 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // 31x2 = 62
 
-// Write your code here
+let random = Math.floor(Math.random() * 100) + 1; // Generates a random integer between 1 and 100
+
+let num3 = 14;
+let num4 = 10;
+let mod = num3 % num4;
+
+let max = Math.max(5, 10, 15, 20); // Finds the maximum number in the set [5, 10, 15, 20], which is 20
+
